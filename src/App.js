41ecfce27c8ac5d72b132122f81b8152a17c9429
booban1 <simonnyongesa6@gmail.com>
2023-01-26 17:02:47 +0300

    layout push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
+import biceps from './biceps.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Weekly Member Perfomance rating
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={biceps} className="App-logo" alt="logo" />
+        <hr />
+        <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
     </div>
   );
 }
